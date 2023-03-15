fada9dcd67450659bc395c4d73c915b12aa97599
jarred wilson <20207921+jardon@users.noreply.github.com>
2023-03-14 21:26:22 -0400

    Fix incorrect artifact name on publish action (#13)
    - Fix incorrect artifact name on publish action
    
    - Update name to charmed-mongodb
    
    - Update snap release to 5/edge
    
    - Update tox environments
    
    - Move to ghcr
    
    * Rework build process
    
    * Move publish to PAT

---
+++ retag_rock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+NAME=$(yq '.name' rockcraft.yaml)
+APP_VERSION=$(yq '.version' rockcraft.yaml)
+VERSION=$(yq '(.version|split("-"))[0]' rockcraft.yaml)
+BASE=$(yq '(.base|split(":"))[1]' rockcraft.yaml)
+TAG=${VERSION}-${BASE}_edge
+REGISTRY_NAMESPACE=ghcr.io/canonical
+
+cat ${NAME}_${APP_VERSION}_amd64.rock | docker import - ${REGISTRY_NAMESPACE}/${NAME}:${TAG}
+docker save ${REGISTRY_NAMESPACE}/${NAME}:${TAG} -o ${NAME}_${TAG}_amd64.rock
