a7a82470818d39ea9fe94b2df3ead1253a81306d
Neha Oudin <neha@oudin.red>
2025-01-20 17:00:35 +0100

    fix: almost empty commit for new hash

--- scripts/start-mongod.sh
+++ scripts/start-mongod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# This starts mongod with the args from the environment
 exec /usr/bin/mongod ${MONGOD_ARGS}
--- scripts/start-mongos.sh
+++ scripts/start-mongos.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# This starts mongos with the provided arguments
 exec /usr/bin/mongos ${MONGOS_ARGS}
